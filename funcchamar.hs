54b7e2479b9ad04edc19ac383a9f86ce1ff0fa62
d10022004 <david.fidalgo1010@hotmail.com>
2023-05-06 16:30:11 +0100

    Update funcchamar.hs

--- funcchamar.hs
+++ funcchamar.hs
@@ -1,32 +1,27 @@
 module Funcchamar where
 import System.IO 
 
-import Exem (apreuc, apreal)
+
 chamaselecop ::  Int -> IO()
 chamaselecop x 
     |x==1 = do
         putStrLn "Qual o nome da cadeira?"
         uc <- getLine
-        uclist <- apreuc
         putStrLn "Pressione Enter para continuar..."
         hFlush stdout
         getLine
-        main
     |x==2 = do
         putStrLn "Qual o numero do aluno?(alxxx)"
         al <- getLine
-        alunos <- apreal
         putStrLn "Pressione Enter para continuar..."
         hFlush stdout
         getLine
-        main
-    |x==0 = main
+    |x==0 = Nothing
     |otherwise = do 
         putStrLn "OPCAO INVALIDA"
         putStrLn "Pressione Enter para continuar..."
         hFlush stdout
         getLine
-        main
 
 chamaselec :: IO()
 chamaselec = do
